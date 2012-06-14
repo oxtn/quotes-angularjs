d78ee5a06f6e2cc74dae92d667799b70fe3b4297
Austin Rasmussen <ar@austinrasmussen.com>
2012-06-14 14:29:29 +0200

    Oh, mod_rewrite...  Made HTML5 routing work

--- quotes_angularjs/static/js/controllers.js
+++ quotes_angularjs/static/js/controllers.js
@@ -5,10 +5,10 @@ angular.module('quotes', [], ['$routeProvider','$interpolateProvider', '$locatio
     var base_url = '/quotes-ang';    
 
     $routeProvider.
-/*        when(base_url+'/view/:id', { 
-            templateUrl: base_url + '/view',
+        when(base_url+'/view/:id', { 
+            templateUrl: base_url + '/Zview.html',
             controller: QuoteViewCtrl
-       }).*/when(base_url+'/list', {
+       }).when(base_url+'/list', {
             templateUrl: base_url + '/ZOMG.html',
             controller: QuoteListCtrl
        });
@@ -18,10 +18,10 @@ angular.module('quotes', [], ['$routeProvider','$interpolateProvider', '$locatio
 
 function QuoteListCtrl($scope, $http) {
     $scope.loaded = false;
-/*    $http.get('list.json').success(function(data) {
-        $scope.quotes = data;*/
+    $http.get('list.json').success(function(data) {
+        $scope.quotes = data;
         $scope.loaded = true;
-//    });
+    });
 };
 QuoteListCtrl.$inject = ['$scope','$http'];
 
